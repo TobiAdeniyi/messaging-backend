b2ef5831775abf30d50c915782b9f5f2b6fcccfc
Tobi Adeniyi <tobiadeniyi@meta.com>
2023-05-24 15:28:06 +0100

    Create: create tests

---
+++ test/db.test.js
@@ -0,0 +1,6 @@
+const { sequelize } = require("../db/db");
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Connection has been established successfully."))
+  .catch((error) => console.error("Unable to connect to the database:", error));
